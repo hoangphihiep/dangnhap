203663be11256f3e8c7d61e05be943057c195edd
hoangphihiep <phihiep31012004@gmail.com>
2024-01-28 15:45:34 +0700

    Them nhan vien

---
+++ Nhanvien.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baithuchanh3_freeuts
+{
+    public class Nhanvien
+    {
+        //thong tin cua nhan vien
+    }
+}
